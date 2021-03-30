9fd254a84a5a6e37c81859564e3f1909f8154f59
KellyPorter02 <porter.kelly02@gmail.com>
2021-03-29 22:28:44 -0400

    50% complete

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,31 +1,74 @@
 package com.zipcodewilmington.arrayutility;
 
+import com.sun.tools.javac.util.ArrayUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by leon on 3/6/18.
  */
 public class ArrayUtility<T>  {  // created a generic class here
-    
-    
+
+    T[] inputArray;
+
     public ArrayUtility() {} 
 
-    public ArrayUtility(T[] inputArray) {}  // constructor takes a generic type of array
+    public ArrayUtility(T[] inputArray) {  // constructor takes a generic type of array
+        this.inputArray = inputArray;
+    }
 
 
     public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
-        return null;
+//        int newLength = this.inputArray.length + arrayToMerge.length;
+//
+//        T[] mergedArray = Arrays.copyOf(inputArray, newLength);
+//
+////        inputArray = new <~>[newLength];
+//        int counter = 0;
+//        for (T element : inputArray) {
+//            if (element.equals(valueToEvaluate)) {
+//                counter++;
+//            }
+//        }
+//        for (T element: arrayToMerge) {
+//            if (element.equals(valueToEvaluate)) {
+//                counter++;
+//            }
+//        }
+//        return counter;
+        Integer counter = this.getNumberOfOccurrences(valueToEvaluate);
+        for (T element : arrayToMerge) {
+            if (element.equals(valueToEvaluate)) {
+                counter++;
+            }
+        }
+        return counter;
     }
 
     public T getMostCommonFromMerge(T[] arrayToMerge) {
+
         return null;
     }
 
     public Integer getNumberOfOccurrences(T valueToEvaluate) {
-        return null;
+        int counter = 0;
+        for (T element: inputArray) {
+            if (element.equals(valueToEvaluate)) {
+                counter++;
+            }
+        }
+        return counter;
     }
 
     public T[] removeValue(T valueToRemove) {
+
         return null;
     }
+
+//    public T[] mergeArrays(T[] array1, T[] array2) {
+//
+//    }
 
   
 }
